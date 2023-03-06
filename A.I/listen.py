aa63ee3a0efda74ff12ca876c5dd4863f815ea1b
NeverwhereNowhere <manpinas14@gmail.com>
2023-03-06 12:47:41 -0300

    Subo la resolución de los ejercicios

---
+++ A.I/listen.py
@@ -0,0 +1,33 @@
+import speech_recognition
+
+def main():
+    words = input("Saying something: ").lower()
+
+    if "hello" in words:
+        print("Hello to you too!")
+    elif "how are you" in words:
+        print("I am well, thanks")
+    elif "goodbye" in words:
+        print("Goodbye to you too!")
+    else:
+        print("Huh?")
+
+def main1():
+    recognizer = speech_recognition.Recognizer()
+    with speech_recognition.Microphone() as source:
+        print("Say something: ")
+        audio = recognizer.listen(source)
+
+    print("You said: ")
+    print(recognizer.recognize_google(audio))
+
+    words = recognizer.recognize_google(audio)
+
+    if "hello" in words:
+        print("Hello to you too!")
+    elif "how are you" in words:
+        print("I am well, thanks")
+    elif "goodbye" in words:
+        print("Goodbye to you too!")
+    else:
+        print("Huh?")
